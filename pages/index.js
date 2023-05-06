7e44d87a914860080714f9a1dbdb99ee9b28e907
LembergsV1 <118716480+LembergsV1@users.noreply.github.com>
2023-05-06 07:49:06 -0700

    new stuff

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session){
-    return (<div className="bg-blue-900 w-screen h-screen flex items-center">
-              <div className="text-center w-full">
-                <button onClick={() => signIn('google')} className="bg-white p-2 rounded-lg">Login with Google</button>
-              </div>
-            </div>)
-  }
-  return (
-    <div>logged in {session.user.email}</div>
-  )
+  const {data: session}= useSession();
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      {session ? (
+        <>
+          <h2>
+            Hello, <b>{session.user.name}</b>
+          </h2>
+          <div className="flex bg-gray-400 gap-1 text-black rounded-lg overflow-hidden">
+            <img src={session.user.image} alt="" className="w-8 h-8"/>
+            <span className="py-1 px-2">
+              {session.user.name}
+            </span>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  </Layout>
 }
